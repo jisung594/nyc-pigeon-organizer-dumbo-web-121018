b99a1ce05082d61c1a461b30cff22ea8e3a4a384
jonathan choi <jisung594@gmail.com>
2018-12-09 05:31:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each do |attribute,info|
+    info.each do |info_type,names_list|
+      names_list.each do |name|
+         #binding.pry
+        if data[attribute][info_type].include?(name)
+          info_hash = {}
+          pigeon_list[name] = info_hash
+          info_hash[attribute] = [info.keys]
+        end
+      end
+    end
+  end
+  
+  return pigeon_list
 end
